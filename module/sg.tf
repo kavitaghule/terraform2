f10b5c3fe3760855e8970de91f87d5563201a232
kavitaghule <110611046+kavitaghule@users.noreply.github.com>
2023-09-11 11:48:53 +0530

    Create sg.tf

---
+++ module/sg.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "sg2" {
+  name        = "sg2"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  dynamic "ingress" {
+    for_each = [22, 80, 443, 3306, 2049]
+    iterator = port
+    content {
+      description = "tls from vpc"
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg2"
+  }
+}
