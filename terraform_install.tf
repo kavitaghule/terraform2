859b444a0b273b74fb878abeb08901033cdff17e
kavitaghule <110611046+kavitaghule@users.noreply.github.com>
2023-09-11 11:50:15 +0530

    Create terraform_install.tf

---
+++ terraform_install.tf
@@ -0,0 +1,4 @@
+# how to innstall terraform
+sudo yum install -y yum-utils shadow-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
