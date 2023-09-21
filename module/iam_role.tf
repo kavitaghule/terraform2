b15b5b8e58a91ba0242e3c995340538cc13076a3
kavitaghule <110611046+kavitaghule@users.noreply.github.com>
2023-09-21 16:49:03 +0530

    Create iam_role.tf

---
+++ module/iam_role.tf
@@ -0,0 +1,43 @@
+resource "aws_iam_instance_profile" "e22c2_profile" {
+  name = "e22c2_profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name        = "ec2_full_access"
+    Environment = "dev"
+  }
+}
+resource "aws_iam_role_policy" "ec2_policy" {
+  name = "ec2_policy"
+  role = aws_iam_role.ec2_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
