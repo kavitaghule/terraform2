e2858e99bda0263a4dfaf970aee1dfb5b88bb891
kavitaghule <110611046+kavitaghule@users.noreply.github.com>
2023-09-25 22:28:03 +0530

    Update key.tf

--- module/key.tf
+++ module/key.tf
@@ -1,3 +1,11 @@
+# no need to make key if already exist 
+# in instace specify keyname which in present on aws console
+#vi demo.pem = copy key in that
+#chmod 400 demo.pem
+#ssh -i ....
+
+
+
 resource "aws_key_pair" "demo" {
   key_name   = "demo"
   public_key = file("/root/.ssh/id_rsa.pub")
