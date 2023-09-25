a7b2b3fa52f747c90c3e8939e729c4aa964acdf7
kavitaghule <110611046+kavitaghule@users.noreply.github.com>
2023-09-25 22:23:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,15 +4,6 @@ provider "aws" {
   access_key = ""
 }
 module "aws_instance" {
-  source = "/opt/project/module/"
-}
-module "vpc" {
-  source = "/opt/project/module/"
-}
-module "sg" {
-  source = "/opt/project/module/"
-}
-module "key_pair" {
-  source = "/opt/project/module/"
+  source = "./module/"
 }
 
