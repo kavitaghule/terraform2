7776897a018451ae435ae51dbae90e79f65acb8c
kavitaghule <110611046+kavitaghule@users.noreply.github.com>
2023-09-25 18:00:18 +0530

    Update instance.tf

--- module/instance.tf
+++ module/instance.tf
@@ -2,6 +2,8 @@ resource "aws_instance" "web" {
   ami           = "ami-0700df939e7249d03"
   instance_type = "t2.micro"
   key_name="demo"
+subnet_id = aws_subnet.public_sub1.id
+security_groups      = [aws_security_group.sg2.id]
   tags = {
     Name = "web"
   }
