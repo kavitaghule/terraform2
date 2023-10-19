cb4ef4c85ef8974c082d044f46b9a6035b6b4a02
kavitaghule <110611046+kavitaghule@users.noreply.github.com>
2023-10-19 09:53:10 +0530

    Update terraform_install.sh

--- terraform_install.sh
+++ terraform_install.sh
@@ -2,3 +2,11 @@
 sudo yum install -y yum-utils shadow-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
+
+
+# install hashicorp
+sudo yum -y install vault
+vault server -dev
+#Unseal Key: PLV0OXO9VmF5VB8qAnq4pQIGzWkzzYypRNcDtrhSSgU=
+#Root Token: hvs.6j4cuewowBGit65rheNoceI7
+export VAULT_ADDR='http://127.0.0.1:8200'
